6714691fc89a2bf49219ff734af7f8e2ce571066
Woogie95 <choisw950928@gmail.com>
2023-07-24 21:07:14 +0900

    Initial commit

---
+++ src/test/java/com/example/bootloanservice/BootLoanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootloanservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootLoanServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
