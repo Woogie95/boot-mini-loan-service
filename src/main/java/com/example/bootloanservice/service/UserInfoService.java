aac861549cb82c34046deec2fcf8286c8af29447
Woogie95 <choisw950928@gmail.com>
2023-07-30 16:42:48 +0900

    feat: ing

--- src/main/java/com/example/bootloanservice/service/UserInfoService.java
+++ src/main/java/com/example/bootloanservice/service/UserInfoService.java
@@ -2,11 +2,17 @@ package com.example.bootloanservice.service;
 
 import com.example.bootloanservice.config.UniqueKey;
 import com.example.bootloanservice.dto.CreateUserInfoDTO;
+import com.example.bootloanservice.dto.FindByOrganizationCodeProductInfoDTO;
+import com.example.bootloanservice.entity.ProductInfo;
 import com.example.bootloanservice.entity.UserInfo;
 import com.example.bootloanservice.repository.UserInfoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +20,7 @@ public class UserInfoService {
 
     private final UserInfoRepository userInfoRepository;
 
+    @Transactional
     public String createUserInfo(CreateUserInfoDTO.Request createUserInfoDTO) {
         String userKey = UniqueKey.generateUniqueKey();
 
@@ -29,7 +36,16 @@ public class UserInfoService {
         return CreateUserInfoDTO.Response.from(userInfo).getUserKey();
     }
 
+    @Transactional(readOnly = true)
     public ResponseEntity<?> findByUserKey(String userKey) {
 
     }
 }
+    @Transactional(readOnly = true)
+    public List<FindByOrganizationCodeProductInfoDTO> findByOrganizationCode(String organizationCode) {
+        List<ProductInfo> productInfos = productInfoRepository.findByOrganizationCode(organizationCode);
+
+        return productInfos.stream()
+                .map(FindByOrganizationCodeProductInfoDTO::from)
+                .collect(Collectors.toList());
+    }
